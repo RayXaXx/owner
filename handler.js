838577d62c5d3387f5cfc4a3c0037110aeacf5ba
RayXaXx <134533024+RayXaXx@users.noreply.github.com>
2023-05-28 12:39:15 -0500

    Add files via upload

--- handler.js
+++ handler.js
@@ -85,7 +85,7 @@ global.db.data.chats[m.chat] = {}
                 
 if (chat) {
 if (!('isBanned' in chat)) chat.isBanned = false                    
-if (!('welcome' in chat)) chat.welcome = true                    
+if (!('welcome' in chat)) chat.welcome = false                   
 if (!('detect' in chat)) chat.detect = true                    
 if (!('sWelcome' in chat)) chat.sWelcome = ''                    
 if (!('sBye' in chat)) chat.sBye = ''                    
@@ -111,7 +111,7 @@ if (!isNumber(chat.expired)) chat.expired = 0
 } else
 global.db.data.chats[m.chat] = {
 isBanned: false,
-welcome: true,
+welcome: false,
 detect: true,
 sWelcome: '',
 sBye: '',
